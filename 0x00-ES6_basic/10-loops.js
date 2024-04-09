eb98c208f0ef7013dcc7e45d006ae67048838302
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-09 14:17:21 +0000

    Done

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const returnArray = [];
   for (const idx of array) {
     returnArray.push(appendString + idx);
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,4 +1,4 @@
-export default function createReportObject (employeesList) {
+export default function createReportObject(employeesList) {
   const getNumberOfDepartments = () => Object.keys(employeesList).length;
   return { allEmployees: employeesList, getNumberOfDepartments };
 }
