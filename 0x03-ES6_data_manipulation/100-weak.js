0dd057ab59a936746bdeec6b449c34e3f08966e1
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-15 20:53:11 +0100

    100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+const weakMap = new WeakMap();
+
+let count = 0;
+
+function queryAPI(endpoint) {
+  weakMap.set(endpoint, count += 1);
+  const callsNum = weakMap.get(endpoint);
+  if (callsNum >= 5) {
+    throw new Error('Endpoint load is high.');
+  }
+}
