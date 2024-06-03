091cfb1203f100643f34d48cfb11aa05df15e0d6
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-03 19:13:19 +0100

    1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+    process.exit();
+  }
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
