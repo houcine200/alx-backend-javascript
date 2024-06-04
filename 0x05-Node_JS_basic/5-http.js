8084c4d30fdd14db29546f60d5ec066d28892253
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-04 18:56:06 +0100

    fix npx

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');  // Adjust the import to the async version
+const countStudents = require('./3-read_file_async'); // Adjust the import to the async version
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,7 +9,7 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    
+
     countStudents(process.argv[2])
       .then((studentData) => {
         res.end(studentData);
