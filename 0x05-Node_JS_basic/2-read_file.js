91eb50cf11ce680384a1c3cae411603ac40698fa
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-04 12:33:21 +0100

    2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(fileName) {
+   
+    const data = fs.readFileSync(fileName, 'utf8');
+    
+    const arr = data.split('\n');
+    const totalStudents = arr.length - 1;
+    console.log(`Number of students: ${totalStudents}`)
+
+    const header = arr[0].split(',');
+    const fieldIndex = header.indexOf('field'); 
+
+    let countCS = 0;
+    let countSWE = 0;
+    let csNames = [];
+    let sweNames = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        const columns = arr[i].split(',');
+        const field = columns[fieldIndex].trim();
+        const firstName = columns[0].trim();
+
+        if (field === 'CS') {
+            csNames.push(firstName);
+            countCS++;
+        } else if (field === 'SWE') {
+            sweNames.push(firstName);
+            countSWE++;
+        }
+    }
+    console.log(`Number of students in CS: ${countCS}. List: ${csNames.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSWE}. List: ${sweNames.join(', ')}`);
+};
+
+module.exports = countStudents;
