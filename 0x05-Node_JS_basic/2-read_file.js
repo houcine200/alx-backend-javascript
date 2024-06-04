53bf98a7b16b4412ec109a45b8caa1275013107f
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-04 12:49:17 +0100

    2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,39 @@
 const fs = require('fs');
 
 function countStudents(fileName) {
-  const data = fs.readFileSync(fileName, 'utf8');
+  try {
+    const data = fs.readFileSync(fileName, 'utf8');
 
-  const arr = data.split('\n');
-  const totalStudents = arr.length - 1;
-  console.log(`Number of students: ${totalStudents}`);
+    const arr = data.split('\n');
+    const totalStudents = arr.length - 1;
+    console.log(`Number of students: ${totalStudents}`);
 
-  const header = arr[0].split(',');
-  const fieldIndex = header.indexOf('field');
+    const header = arr[0].split(',');
+    const fieldIndex = header.indexOf('field');
 
-  let countCS = 0;
-  let countSWE = 0;
-  const csNames = [];
-  const sweNames = [];
+    let countCS = 0;
+    let countSWE = 0;
+    const csNames = [];
+    const sweNames = [];
 
-  for (let i = 1; i < arr.length; i += 1) {
-    const columns = arr[i].split(',');
-    const field = columns[fieldIndex].trim();
-    const firstName = columns[0].trim();
+    for (let i = 1; i < arr.length; i += 1) {
+      const columns = arr[i].split(',');
+      const field = columns[fieldIndex].trim();
+      const firstName = columns[0].trim();
 
-    if (field === 'CS') {
-      csNames.push(firstName);
-      countCS += 1;
-    } else if (field === 'SWE') {
-      sweNames.push(firstName);
-      countSWE += 1;
+      if (field === 'CS') {
+        csNames.push(firstName);
+        countCS += 1;
+      } else if (field === 'SWE') {
+        sweNames.push(firstName);
+        countSWE += 1;
+      }
     }
+    console.log(`Number of students in CS: ${countCS}. List: ${csNames.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSWE}. List: ${sweNames.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-  console.log(`Number of students in CS: ${countCS}. List: ${csNames.join(', ')}`);
-  console.log(`Number of students in SWE: ${countSWE}. List: ${sweNames.join(', ')}`);
 }
 
 module.exports = countStudents;
