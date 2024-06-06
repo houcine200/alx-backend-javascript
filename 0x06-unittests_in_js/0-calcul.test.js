3eec21935b85e381acec5bbd04f2e190bedc17c8
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-06 19:05:00 +0100

    0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return rounded numbers sum = 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return rounded numbers sum = 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return rounded numbers sum = 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
