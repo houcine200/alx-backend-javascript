2dc3b279455e628cd1b3a9203239f57a4e0b1335
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-07 18:09:07 +0100

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,15 +1,23 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should return rounded numbers sum = 4', () => {
+  it('should return 4 when inputs are 1 and 3', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-  it('should return rounded numbers sum = 5', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return rounded numbers sum = 6', () => {
+});
+
+it('should return 5 when inputs are 1.4 and 3.6', function() {
+    assert.strictEqual(calculateNumber(1.4, 3.6), 5);
+});
+
+it('should return 6 when inputs are 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+});
+  it('should return -4 when inputs are -1 and -2.7', () => {
+    assert.strictEqual(calculateNumber(-1, -2.7), -4);
+  });
+
+  it('should return -4 when inputs are -1.7 and -2', () => {
+    assert.strictEqual(calculateNumber(-1.7, -2), -4);
   });
 });
